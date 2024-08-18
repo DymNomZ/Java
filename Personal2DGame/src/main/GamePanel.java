5f212c68226c24e9363776312e0e9cd2339debcc
DymNomZ <dymierborgonia@gmail.com>
2024-08-18 18:05:39 +0800

    Basic functionalities added

--- Personal2DGame/src/main/GamePanel.java
+++ Personal2DGame/src/main/GamePanel.java
@@ -2,6 +2,8 @@ package main;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements Runnable{
@@ -14,12 +16,22 @@ public class GamePanel extends JPanel implements Runnable{
     final int screenWidth = tileSize * maxScreenCol;
     final int screenHeight = tileSize * maxScreenRow;
 
+    int FPS = 60;
+
+    KeyHandler keyHandler = new KeyHandler();
     Thread gameThread;
+
+    //Default player coords:
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyHandler);
+        this.setFocusable(true);
     }
 
     public void startGameThread(){
@@ -29,7 +41,55 @@ public class GamePanel extends JPanel implements Runnable{
 
     @Override
     public void run(){
-        
+        double drawInterval = 1000000000/FPS, delta = 0;
+        long lastTime = System.nanoTime(), currentTime;
+        long timer = 0;
+        int drawCount = 0;
+
+        while(gameThread != null){
+
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            if(delta >= 1){
+                updatePlayerLoc();
+                repaint();
+                delta--;
+                drawCount++;
+            }
+
+            if(timer >= 1000000000){
+                System.out.println("FPS: " + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+        }
+    }
+
+    public void updatePlayerLoc(){
+        //Could separate elses in different ifs
+        if(keyHandler.upPressed){
+            playerY -= playerSpeed;
+        }else if(keyHandler.leftPressed){
+            playerX -= playerSpeed;
+        }else if(keyHandler.downPressed){
+            playerY += playerSpeed;
+        }else if(keyHandler.rightPressed){
+            playerX += playerSpeed;
+        }
+    }
+
+    @Override
+    public void paintComponent(Graphics graphics){
+        super.paintComponent(graphics);
+
+        Graphics graphics2D = (Graphics2D)graphics;
+
+        graphics2D.setColor(Color.white);
+        graphics2D.fillRect(playerX, playerY, tileSize, tileSize);
+        graphics2D.dispose();
     }
 
 
